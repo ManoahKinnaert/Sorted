a95de80ed36279bcfa0ef708634810c18d7813f4
Manoah Kinnaert <manoah.k@icloud.com>
2025-11-24 12:55:38 +0100

    Added basic UI behaviour

--- Sorted/js/index.js
+++ Sorted/js/index.js
@@ -1,30 +1,34 @@
-let insertion_btn = document.getElementById("insertion_btn")
-let selection_btn = document.getElementById("selection_btn")
-let quick_btn = document.getElementById("quick_btn")
-let merge_btn = document.getElementById("merge_btn")
-
-let reset_btn = document.getElementById("reset_btn")
+let sorting_btns = {
+    "insertion_btn": document.getElementById("insertion_btn"), 
+    "selection_btn": document.getElementById("selection_btn"),
+    "quick_btn": document.getElementById("quick_btn"),
+    "merge_btn": document.getElementById("merge_btn")
+}
 let sort_btn = document.getElementById("sort_btn")
+let reset_btn = document.getElementById("reset_btn")
 
-insertion_btn.onclick = () => {select_insertion_sort()}
-selection_btn.onclick = () => {select_selection_sort()}
-quick_btn.onclick = () => {select_quick_sort()}
-merge_btn.onclick = () => {select_merge_sort()}
+sorting_btns["insertion_btn"].onclick = () => {select_sort_type("insertion_btn")}
+sorting_btns["selection_btn"].onclick = () => {select_sort_type("selection_btn")}
+sorting_btns["quick_btn"].onclick = () => {select_sort_type("quick_btn")}
+sorting_btns["merge_btn"].onclick = () => {select_sort_type("merge_btn")}
 sort_btn.onclick = () => {do_sort()}
+reset_btn.onclick = () => {reset()}
 
-function select_insertion_sort() {
-    sort_btn.disabled = false
-    alert("test")
+function select_sort_type(type) {
+    sort_btn.disabled = false  
 }
 
-function select_selection_sort() {
-}
-
-function select_quick_sort() {
-}
-
-function select_merge_sort() {
+function reset() {
+   for (const [key, btn] of Object.entries(sorting_btns)) btn.disabled = false
+   sort_btn.disabled = true 
 }
 
 function do_sort() {
+    console.log("[DEBUG]: Starting sort")
+    sort_btn.disabled = true
+    reset_btn.disabled = true
+    for (const [key, btn] of Object.entries(sorting_btns)) btn.disabled = true
+    // TODO: perform the sort
+    
+    reset_btn.disabled = false 
 }

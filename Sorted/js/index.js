d01c0b91cc4507dd766a037f38544b5249e14809
Manoah Kinnaert <manoah.k@icloud.com>
2025-11-25 16:49:20 +0100

    Implemented selection sort without proper animation

--- Sorted/js/index.js
+++ Sorted/js/index.js
@@ -1,3 +1,9 @@
+import { DataArray } from './data.js'
+import {selectionSort} from './sorted.js'
+
+let dataArray = new DataArray(null)
+let sortType = ""
+
 let sorting_btns = {
     "insertion_btn": document.getElementById("insertion_btn"), 
     "selection_btn": document.getElementById("selection_btn"),
@@ -14,13 +20,31 @@ sorting_btns["merge_btn"].onclick = () => {select_sort_type("merge_btn")}
 sort_btn.onclick = () => {do_sort()}
 reset_btn.onclick = () => {reset()}
 
+window.onload = () =>  {
+    renderTable()
+}
+
+function renderTable() {
+    
+    dataArray.generateArray(40, 10, 40)
+    let table = document.getElementById("array-container")
+    let html = ""
+    html += dataArray.getHtml()
+
+    table.innerHTML = html
+}
+
+
 function select_sort_type(type) {
-    sort_btn.disabled = false  
+    sort_btn.disabled = false
+    sortType = type
+
 }
 
 function reset() {
    for (const [key, btn] of Object.entries(sorting_btns)) btn.disabled = false
-   sort_btn.disabled = true 
+   sort_btn.disabled = true
+   renderTable() 
 }
 
 function do_sort() {
@@ -29,6 +53,10 @@ function do_sort() {
     reset_btn.disabled = true
     for (const [key, btn] of Object.entries(sorting_btns)) btn.disabled = true
     // TODO: perform the sort
+    console.log(sortType)
+    if (sortType === "selection_btn") {
+        selectionSort(dataArray)
+    }
     
     reset_btn.disabled = false 
 }
---
+++ Sorted/js/sorted.js
@@ -0,0 +1,36 @@
+function findMinimumPos(values, start) {
+    let minPos = start
+    for (let i=start + 1; i < values.size; i++) {
+        if (values.getElement(i) < values.getElement(minPos)) {
+            minPos = i
+        }
+    }
+    return minPos
+}
+
+function selectionSort(values) {
+    for (let i = 0; i < values.size; i++) {
+        let minPos = findMinimumPos(values, i)
+        // swap the elements
+        let temp = values.getElement(minPos)
+        values.setElement(values.getElement(i), minPos)
+        values.setElement(temp, i)
+    }
+}
+
+function insertionSort(values) {
+    for (let i=1; i < values.size; i++) {
+        let nextVal = values.getElement(i)
+        let j = i 
+        while (j > 0 && values.getElement(j - 1) > nextVal) {
+            values.setElement(values.getElement(j - 1), j)
+            j -= 1
+        }
+        values.setElement(nextVal, j)
+    }
+}
+// TODO: to be implemented
+function quickSort() {
+}
+
+export {selectionSort}

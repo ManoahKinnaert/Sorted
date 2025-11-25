5cd567f0740ad69ae08988c744c4759c82e2d1b5
Manoah Kinnaert <manoah.k@icloud.com>
2025-11-25 16:49:20 +0100

    Implemented selection sort without propper animation

--- Sorted/js/data.js
+++ Sorted/js/data.js
@@ -1,14 +1,30 @@
-class DataObject {
-    constructor (value, index) {
-        this.value = value 
-        this.index = index 
-    }
-}
-
 class DataArray {
-    constructor (values=null) {
-        if (values == null) { this.values = [] }
-        else { this.values = values } 
+    constructor (size) {
+        this.size = size
+        this.values = []
+
+    }
+
+    getHtml() {
+        let html = ""
+        for(let i = 0; i < this.values.length; i++) {
+            html += `<td> <div id="${i}" style="width: 20px; height: ${this.values[i] * 10}px;"></div></td>`
+        }
+        return html
+    }
+
+    updateHtml() {
+        for (let i=0; i < this.values.length; i++) {
+            document.getElementById(`${i}`).style.height= `${this.values[i] * 10}px`
+        }
+    }
+
+    generateArray(size, low, high) {
+        this.values = []
+        this.size = size 
+        for (let i = 0; i < this.size; i++) {
+            this.values.push(Math.floor(Math.random() * (high - low + 1)) + low)
+        }
     }
 
     pushElement(element) {
@@ -17,9 +33,13 @@ class DataArray {
 
     setElement(element, index) {
         this.values[index] = element 
+        setTimeout(() => {document.getElementById(`${index}`).style.height = `${this.values[index] * 10}px`}, 100)
     }
 
     getElement(index) {
+        setTimeout(() => {}, 500) 
         return this.values[index]
     }
 }
+
+export {DataArray}
